df2ef288e4116f49502153a3f1c31c42542c4d0a
JakeCrompton <jakecrompton06@gmail.com>
2025-10-29 03:41:56 +0000

    small change

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-# Creating this for now
-# TEST
-#TEST
+# If the script doesnt work (says the file doesnt exist or sum) type in "cd yourfilelocation" in the output
+
+import json
+
+with open("mapLoader.json", "r") as file:
+    data = json.load(file)
+
+print(data)
