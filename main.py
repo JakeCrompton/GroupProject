e61b942c2b64668077041dfdf3e293c3e63c6763
JakeCrompton <jakecrompton06@gmail.com>
2025-10-31 05:07:12 +0000

    able to find the json file

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # If the script doesnt work (says the file doesnt exist or sum) type in "cd yourfilelocation" in the output
 
-import json
+import json, os
 
-with open("mapLoader.json", "r") as file:
+# Find the file location
+base_path = os.path.dirname(__file__)
+file_path = os.path.join(base_path, "mapLoader.json")
+
+with open(file_path, "r") as file:
     data = json.load(file)
 
 print(data)
