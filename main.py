dfbb392fd20dcc92f5ea880a1d3209eb03810e78
JakeCrompton <jakecrompton06@gmail.com>
2025-10-31 20:41:31 +0000

    changed maploader for better formatting

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # If the script doesnt work (says the file doesnt exist or sum) type in "cd yourfilelocation" in the output
+# means we can change stuff inside the map and it would work as it would as if it was something else (Data wise)
 
 import json, os
 
 # Find the file location
 base_path = os.path.dirname(__file__)
-file_path = os.path.join(base_path, "mapLoader.json")
+mapFile = os.path.join(base_path, "mapLoader.json")
 
-with open(file_path, "r") as file:
-    data = json.load(file)
+# opens the json file
+with open(mapFile, "r") as file:
+    mapData = json.load(file)
 
-print(data)
+
+print(f"The map data: {mapData}")
