7a8dd4809523a821cfcc3561ebd1765d8ca6f6c9
JakeCrompton <jakecrompton06@gmail.com>
2025-11-01 01:30:01 +0000

    base model sorted (proof of concept)

--- main.py
+++ main.py
@@ -3,13 +3,35 @@
 
 import json, os
 
-# Find the file location
+# load the json map file
 base_path = os.path.dirname(__file__)
 mapFile = os.path.join(base_path, "mapLoader.json")
 
-# opens the json file
+# reads the json file
 with open(mapFile, "r") as file:
     mapData = json.load(file)
 
+# get the players starting location and inventory
+player_location = mapData["player"]["start_location"]
+inventory = mapData["player"]["inventory"]
 
-print(f"The map data: {mapData}")
+print(f"You start in the player {player_location}.")
+print(f"Your inventory {inventory}")
+
+# get info about the new room
+current_room = mapData["Rooms"][player_location]
+print(f"\n{current_room['description']}")
+print(f"Items here: {current_room['items']}")
+print(f"Exits: {list(current_room['exits'].keys())}")
+
+print(f"What direction would you like to go {current_room['exits']}")
+direction = input(">")
+
+
+# check if direction is valid
+if direction in current_room['exits']:
+    new_room = current_room['exits'][direction]
+    player_location = new_room
+    print(f"\nYou move {direction} to the {player_location}")
+else:
+    print("\nYou cannot move that way.")
