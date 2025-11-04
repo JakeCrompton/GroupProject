3c7a53092c10c2171aeadfe8771b46493cb3618d
CharlieManton <cmanton77777@gmail.com>
2025-11-04 17:45:08 +0000

    test 2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # If the script doesnt work (says the file doesnt exist or sum) type in "cd yourfilelocation" in the output
 # means we can change stuff inside the map and it would work as it would as if it was something else (Data wise)
 
-
-import json, os
+# importing Datasets 
+import json, os, pygame
 
 # loads the json map file
 base_path = os.path.dirname(__file__)
