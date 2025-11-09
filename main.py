cf98d715f60d6606d8fa2c104de108b5cf72811d
JakeCrompton <jakecrompton06@gmail.com>
2025-11-09 12:18:41 +0000

    added few small things

--- main.py
+++ main.py
@@ -8,22 +8,35 @@ with open(mapFile, "r") as file:
     mapData = json.load(file) # loads the json file as a variable
 
 player_location = mapData['player']['start_location'] # gets player data
-
 inventory = mapData['player']['inventory']
 
 # Functions
 
-def move(direction, player_location, mapData): # checks if the direction the user entered is valid (using a function so easier to run)
-
+def moveRoom(direction, player_location, mapData): # checks if the direction the user entered is valid (using a function so easier to run)
     current_room = mapData['Rooms'][player_location]
-    
+
     if direction in current_room['exits']:
         new_room = current_room['exits'][direction]
+        next_room_data = mapData['Rooms'][new_room]
+
+        if "required_items" in next_room_data: # checks if there is a required item to go into the next room
+            required_items = next_room_data['required_items']
+            missing = [item for item in required_items if item not in inventory]
+            if missing:
+                print(f"To go into {new_room} you need the following items:")
+                for item in missing:
+                    print(f"- {item}")
+                return player_location
+            
         print(f"You have moved {direction} to {new_room}")
         return new_room
+    
     else:
         print("You cannot move that way")
         return player_location
+    
+def clearOutput():
+    print("This is going to clear the output when this function is called (cba making it rn tho)")
 
 # Main
 round = 0
@@ -31,13 +44,14 @@ round = 0
 while True: 
     current_room = mapData['Rooms'][player_location] # get info about new room
     print(f"\n{current_room['description']}")
-    print(f"Items here: {current_room['items']}")
-    print(f"Exits: {list(current_room['exits'].keys())}")
 
-    print(f"Where would you like to go? {current_room['exits']}")
+    print(inventory) # testing with keys
+    
+    print("Where would you like to go?")
+    for direction, room in current_room['exits'].items():
+        print(f"-    {direction}: {room}")
     direction = input(">").strip().lower()
 
-    player_location = move(direction, player_location, mapData)
+    player_location = moveRoom(direction, player_location, mapData)
     current_room = mapData['Rooms'][player_location]
-    print(f"You are now in {player_location}")
 
