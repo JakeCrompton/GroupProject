e46bd47a0a5a41b80f54e1b65cd901909ce10c0e
JakeCrompton <jakecrompton06@gmail.com>
2025-11-09 11:51:21 +0000

    made the code more readable

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-# If the script doesnt work (says the file doesnt exist or sum) type in "cd yourfilelocation" in the output
-# means we can change stuff inside the map and it would work as it would as if it was something else. Can store map data inside the json file such as drops, npcs, enemies and other shit like that
-
-# pygame stuff is just testing rn it will be changed
-
-# importing json and os so it can find the files + pygame for the window (adding pygame back later it was always running and got in the way)
 import json, os, pygame, sys
 
-# loads the json map file
-base_path = os.path.dirname(__file__)
+base_path = os.path.dirname(__file__) # finds the directory for the files
 mapFile = os.path.join(base_path, "mapLoader.json")
 
-# reads the json file 
+
 with open(mapFile, "r") as file:
-    mapData = json.load(file)
+    mapData = json.load(file) # loads the json file as a variable
 
-# MAIN PART BELOW
+player_location = mapData['player']['start_location'] # gets player data
 
-# rounds of *zombies* that will attack you, you can only move a certain amount of steps inbetween the round but every 5 rounds you can move as much as you want till you choose to resume the game (gives you time to progress with the story, cannot complete the whole story in that time)
+inventory = mapData['player']['inventory']
 
-def move(direction, player_location, mapData):  # can run this function instead of running it each time the player wants to go to a different room
+# Functions
+
+def move(direction, player_location, mapData): # checks if the direction the user entered is valid (using a function so easier to run)
+
     current_room = mapData['Rooms'][player_location]
     
     if direction in current_room['exits']:
@@ -29,20 +25,19 @@ def move(direction, player_location, mapData):  # can run this function instead 
         print("You cannot move that way")
         return player_location
 
-# gets player data
-player_location = mapData['player']['start_location']
-inventory = mapData['player']['inventory']
+# Main
+round = 0
 
-# get info about new room
-current_room = mapData['Rooms'][player_location]
-print(f"\n{current_room['description']}")
-print(f"Items here: {current_room['items']}")
-print(f"Exits: {list(current_room['exits'].keys())}")
+while True: 
+    current_room = mapData['Rooms'][player_location] # get info about new room
+    print(f"\n{current_room['description']}")
+    print(f"Items here: {current_room['items']}")
+    print(f"Exits: {list(current_room['exits'].keys())}")
 
-print(f"Where would you like to go? {current_room['exits']}")
-direction = input(">").strip().lower()
+    print(f"Where would you like to go? {current_room['exits']}")
+    direction = input(">").strip().lower()
 
-player_location = move(direction, player_location, mapData)
-current_room = mapData['Rooms'][player_location]
-print(f"You are now in {player_location}")
-# check if the direction is valid 
+    player_location = move(direction, player_location, mapData)
+    current_room = mapData['Rooms'][player_location]
+    print(f"You are now in {player_location}")
+
